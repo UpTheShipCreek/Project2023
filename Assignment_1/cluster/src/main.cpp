50e8df69689bf556115a090a3da94ccf0c382f81
Chris K <75842353+ChrisK9@users.noreply.github.com>
2023-10-20 02:09:50 +0300

    Added template for clustering main.

---
+++ Assignment_1/cluster/src/main.cpp
@@ -0,0 +1,102 @@
+#include <stdio.h>
+#include <unordered_map>
+#include <queue>
+#include <algorithm>
+#include <climits>
+#include <chrono>
+#include <unistd.h>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <getopt.h>
+using namespace std;
+
+#include "random_functions.h"
+#include "io_functions.h"
+
+
+int main(int argc, char **argv){
+    // ------------------------------------------------------------------- //
+    // --------------------- PROGRAM INITIALIZATIONS --------------------- //
+    // ------------------------------------------------------------------- //
+    int kLSH = 4, L = 5, N = 1;                                             // Default values for lsh
+    int  kHC = 3, M = 10, probes = 2;                                       //Default values for hypercube
+    int completeF;
+    double R = 2000.0;                           
+    int opt;
+    extern char *optarg; 
+    std::string inputFile, configFile, outputFileName, methodChoice;
+    int cmdNecessary = 0;
+    enum methods {classic = 0,lsh,hypercube};
+    methods method;
+    // ------------------------------------------------------------------- //
+    // --------------------- PROGRAM INITIALIZATIONS --------------------- //
+    // ------------------------------------------------------------------- //
+
+
+    // ------------------------------------------------------------------- //
+    // -------------------------- INPUT PARSING -------------------------- //
+    // ------------------------------------------------------------------- //
+    while ((opt = getopt(argc, argv, "i:c:o:complete:m:")) != -1){                   //Parse through (potential) command line arguments
+        switch (opt) {
+            case 'i':                                                               //Files
+                inputFile = optarg;
+                cmdNecessary++;
+                break;
+            case 'c':
+                configFile = optarg;
+                cmdNecessary++;
+                break;
+            case 'o':
+                outputFileName = optarg;
+                cmdNecessary++;
+                break;
+            case 'complete':                                                               //Parameters
+                completeF = 1;
+                break;
+            case 'm':
+                methodChoice = optarg;
+                cmdNecessary++;
+                break;  
+        }
+    }
+
+    if(cmdNecessary != 4){
+        printf("Program execution requires an input file, output file, a cluster config file and a method selection.");
+        return -1;
+    }
+    else{
+        if (methodChoice.compare("Classic") == 0)
+            method = classic;
+        else if (methodChoice.compare("LSH") == 0)
+            method = lsh;
+        else if (methodChoice.compare("Hypercube") == 0)
+            method = hypercube;
+        else {
+            cout << "Error: Method given is not supported or does not exist." << endl;
+            exit(-1);
+        }
+        printf("Program will proceed with values: k = %d, L = %d, N = %d, R = %f\n", k, L, N, R);
+    }
+    // ------------------------------------------------------------------- //
+    // -------------------------- INPUT PARSING -------------------------- //
+    // ------------------------------------------------------------------- //
+
+    // ------------------------------------------------------------------- //
+    // -------------------------- OPEN OUTPUT FILE ----------------------- //
+    // ------------------------------------------------------------------- //
+    FILE* outputFile = fopen(outputFileName.c_str(), "w");
+    // ------------------------------------------------------------------- //
+    // -------------------------- OPEN OUTPUT FILE ----------------------- //
+    // ------------------------------------------------------------------- //
+
+    // ------------------------------------------------------------------- //
+    // -------------------------- CLOSE OUTPUT FILE ---------------------- //
+    // ------------------------------------------------------------------- //
+    fclose(outputFile);
+    // ------------------------------------------------------------------- //
+    // -------------------------- CLOSE OUTPUT FILE ---------------------- //
+    // ------------------------------------------------------------------- //
+
+    return 0;
+}
