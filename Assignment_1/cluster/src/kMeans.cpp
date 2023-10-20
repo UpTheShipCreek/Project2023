5ab77d2fb2e3e1b6a94173fd6f667a07e1d9de51
uptheshipcreek <northenore@gmail.com>
2023-10-20 12:43:05 +0300

    Renamed clustering.h and .cpp  to cluster.h /.cpp and cluster folder to cluster_folder

--- Assignment_1/cluster/src/kMeans.cpp
+++
@@ -1,9 +0,0 @@
-#include <string>
-#include <vector>
-#include <algorithm>
-#include <iterator>
-
-
-std::vector<PointPtr> k_means(std::vector<PointPtr> pointsInput, int numCentroidPoints, int dimensions);
-int choosePoint(std::vector<PointPtr> pointsInput, std::vector<double> distances);
-double minCentroidDist(PointPtr p, std::vector<PointPtr> centroidPoints, int dimensions);
