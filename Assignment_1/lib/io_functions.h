9302e2712062fbf22173e2fc0e2758cacc61021d
Chris K <75842353+ChrisK9@users.noreply.github.com>
2023-10-24 00:40:41 +0300

    Started on cluster output

--- Assignment_1/lib/io_functions.h
+++ Assignment_1/lib/io_functions.h
@@ -12,4 +12,7 @@ std::vector<std::shared_ptr<ImageVector>> read_mnist_images(const std::string& f
 void write_approx_lsh(std::shared_ptr<ImageVector> query, std::vector<std::pair<double, int>> approx, std::vector<std::pair<double, int>> exhaust, double tLSH, double tTrue, FILE* outputFile);
 void write_approx_cube(std::shared_ptr<ImageVector> query, std::vector<std::pair<double, int>> approx, std::vector<std::pair<double, int>> exhaust, double tCube, double tTrue, FILE* outputFile);
 void write_r_near(std::vector<std::pair<double, int>> inRange, int r, FILE* outputFile);
+enum methods {classic = 0, lsh, hypercube};
+
 #endif
+
