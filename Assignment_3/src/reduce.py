26735a995075d17682e7301088674a3c97fd062c
Chris K. <75842353+ChrisK9@users.noreply.github.com>
2023-12-26 21:31:57 +0000

    Minor change

--- Assignment_3/src/reduce.py
+++ Assignment_3/src/reduce.py
@@ -38,7 +38,7 @@ def load_images(file):
 def BuildAutoEncoder():
     #encoder
     inputImage = tf.keras.input(shape=(28,28,1))
-    x = layers.Conv2D(16, (3, 3), activation='relu', padding='same')(input_img)
+    x = layers.Conv2D(16, (3, 3), activation='relu', padding='same')(inputImage)
     x = layers.MaxPooling2D((2, 2), padding='same')(x)
     x = layers.Conv2D(8, (3, 3), activation='relu', padding='same')(x)
     x = layers.MaxPooling2D((2, 2), padding='same')(x)
